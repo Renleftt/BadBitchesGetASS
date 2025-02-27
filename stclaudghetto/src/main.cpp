05deadea86e8e6bea905f729cbe89306e57c0c64
w3cj <cj@null.computer>
2025-02-27 16:41:28 -0600

    rebirth begins

--- stclaudghetto/src/main.cpp
+++ stclaudghetto/src/main.cpp
@@ -69,8 +69,8 @@ void autonomous() {}
 
  
 //Defining the motor groups
-pros::MotorGroup leftMotors({-1, 2, -3, 4});
-pros::MotorGroup rightMotors({5, -6, 7,-8}); 
+pros::MotorGroup leftMotors({1, -2, 3, -4, 5});
+pros::MotorGroup rightMotors({-13, 20,-8,9,-18}); 
 
 //Speed for motors is 100 for now
 void driveForward (int speed) {
@@ -95,14 +95,14 @@ void turnRight(int speed) {
 
 void opcontrol() {
     while (true) {
-                if (master.get_digital(pros::E_CONTROLLER_DIGITAL_UP)) {
-            driveForward(100);  // Drive forward at full speed
+        if (master.get_digital(pros::E_CONTROLLER_DIGITAL_UP)) {
+            driveForward(127);  // Drive forward at full speed
         } else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN)) {
-            driveBackward(100); // Drive backward at full speed
+            driveBackward(127); // Drive backward at full speed
         } else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_LEFT)) {
-            turnLeft(100);      // Turn left at full speed
+            turnLeft(127);      // Turn left at full speed
         } else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_RIGHT)) {
-            turnRight(100);     // Turn right at full speed
+            turnRight(127);     // Turn right at full speed
         } else {
             // Stop all motors if no controls are active
             leftMotors.move_velocity(0);
@@ -142,12 +142,12 @@ void retractPiston() {
 
 void controlPiston() {
     while (true) {
-        if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_L1)) {
+        if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_A)) {
             extendPiston(); // Extend the piston with L1 
-        } else if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_L2)) {
+        } else if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_B)) {
             retractPiston(); // Retract the piston with L2
         }
-        pros::delay(20); // Small delay for button press processing
+        pros::delay(50); // Small delay for button press processing
     }
 }
 
